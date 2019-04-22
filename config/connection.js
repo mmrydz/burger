029e92999727d8a0bc9b853e8573e46df4a041c0
mmrydz <megrydzewski@gmail.com>
2019-04-22 16:55:05 -0400

    toggled out the old var connection

--- config/connection.js
+++ config/connection.js
@@ -11,14 +11,14 @@ if(process.env.JAWSDB_URL) {
       database: 'burger_db'
     });
   };
-  
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "burger_db"
-});
+
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "root",
+//   database: "burger_db"
+// });
 
 // Make connection.
 connection.connect(function(err) {
